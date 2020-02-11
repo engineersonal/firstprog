ecf88a7c89ecab2cae0e80c817809e9ad2a87ecb
Sonal <engineersonal@rediffmail.com>
2020-02-11 22:26:32 +0530

    Initial commit

---
+++ triangle.py
@@ -0,0 +1,9 @@
+for i in range(1,5):
+     for j in range(1,i+1):
+         print("*", end=" ")
+     print()
+
+for k in range(1,5):
+    for l in range(5,k,-1):
+        print("*", end=" ")
+    print()
